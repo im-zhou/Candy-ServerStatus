beffc9844c3e3a144056198396bbd4f1fcdbe417
skyxingcheng233 <skyxingcheng233@outlook.com>
2021-07-17 10:31:58 +0800

    20210717update

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # Candy-ServerStatus 糖果服务器云探针
-2020 07 14
+2021 07 17
 
-# 在线展示 https://icland.xyz/status/
+# 在线展示 https://kvm.icland.xyz/
 
-# 基于修改&优化&去杂
-```bash
-/client/*
-/server/src/main.h
-/server/src/main.cpp
-/web/index.html
-/web/css/hotaru_fix.css
-/web/js/serverstatus.js
-/web/img/*
-```
+![](https://cdn.jsdelivr.net/gh/skyxingcheng/ic-cdn@master/static/pictures/20210717101613.png)
+
+本次更新全面使用了CDN你只需部署服务端和主页即可
+
 # 食用方法:
 ```bash
 git clone https://github.com/skyxingcheng/Candy-ServerStatus.git
@@ -23,13 +17,10 @@ make
 
 客户端使用linux或跨平台的python文件直接启动即可 注意配置服务器和用户名密码
 
-支持ipv4和ipv6支持检测双线
+```
+python client-linux-main.py
+```
 
-需在客户端设置自定义ipv4&ipv6检测地址
+# 本项目修改自:
 
-# 相关开源项目:
-前端来自:		https://github.com/CokeMine/ServerStatus-Hotaru
-
-服务端来自:		https://github.com/BotoX/ServerStatus
-
-客户端来自:		https://github.com/cppla/ServerStatus
+https://github.com/cppla/ServerStatus
--- client/client-psutil-main.py
+++ client/client-psutil-main.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Update by : https://github.com/cppla/ServerStatus
+# Update by : https://github.com/skyxingcheng/Candy-ServerStatus
 # 依赖于psutil跨平台库
 # 支持Python版本：2.7 to 3.7
 # 支持操作系统： Linux, Windows, OSX, Sun Solaris, FreeBSD, OpenBSD and NetBSD, both 32-bit and 64-bit architectures
-# 时间： 20200715
-# 说明: 默认情况下修改server和user就可以了
-
-USER = "user"
-PASSWORD = "user"
+# 时间： 20210717
+# 说明: 默认情况下修改server和user就可以了。丢包率监测方向可以自定义，例如：CU = "www.facebook.com"。
 
 SERVER = "localhost"
+USER = "s02"
+
 PORT = 35601
+PASSWORD = "USER_DEFAULT_PASSWORD"
 INTERVAL = 1
+PROBEPORT = 80
+PROBE_PROTOCOL_PREFER = "ipv4"  # ipv4, ipv6
+PING_PACKET_HISTORY_LEN = 100
+CU = "www.chinaunicom.com"
+CT = "www.chinatelecom.com.cn.iname.damddos.com"
+CM = "gd.10086.cn"
 
 import socket
 import time
 import timeit
 import os
-import json
-import psutil
 import sys
+import json
+import errno
+import psutil
 import threading
+try:
+    from queue import Queue     # python3
+except ImportError:
+    from Queue import Queue     # python2
 
 def get_uptime():
     return int(time.time() - psutil.boot_time())
@@ -66,17 +77,63 @@ def liuliang():
             NET_OUT += v[0]
     return NET_IN, NET_OUT
 
+def tupd():
+    '''
+    tcp, udp, process, thread count: for view ddcc attack , then send warning
+    :return:
+    '''
+    try:
+        if sys.platform.startswith("linux") is True:
+            t = int(os.popen('ss -t|wc -l').read()[:-1])-1
+            u = int(os.popen('ss -u|wc -l').read()[:-1])-1
+            p = int(os.popen('ps -ef|wc -l').read()[:-1])-2
+            d = int(os.popen('ps -eLf|wc -l').read()[:-1])-2
+        elif sys.platform.startswith("win") is True:
+            t = int(os.popen('netstat -an|find "TCP" /c').read()[:-1])-1
+            u = int(os.popen('netstat -an|find "UDP" /c').read()[:-1])-1
+            p = len(psutil.pids())
+            d = 0
+            # cpu is high, default: 0
+            # d = sum([psutil.Process(k).num_threads() for k in [x for x in psutil.pids()]])
+        else:
+            t,u,p,d = 0,0,0,0
+        return t,u,p,d
+    except:
+        return 0,0,0,0
+
+def ip_status():
+    ip_check = 0
+    for i in [CU, CT, CM]:
+        try:
+            socket.create_connection((i, PROBEPORT), timeout=1).close()
+        except:
+            ip_check += 1
+    if ip_check >= 2:
+        return False
+    else:
+        return True
+
 def get_network(ip_version):
     if(ip_version == 4):
-        HOST = "ipv4.google.com"
+        HOST = "mirrors4.tuna.tsinghua.edu.cn"
     elif(ip_version == 6):
-        HOST = "ipv6.google.com"
+        HOST = "mirrors6.tuna.tsinghua.edu.cn"
     try:
         socket.create_connection((HOST, 80), 2).close()
         return True
     except:
         return False
 
+lostRate = {
+    '10010': 0.0,
+    '189': 0.0,
+    '10086': 0.0
+}
+pingTime = {
+    '10010': 0,
+    '189': 0,
+    '10086': 0
+}
 netSpeed = {
     'netrx': 0.0,
     'nettx': 0.0,
@@ -88,27 +145,38 @@ netSpeed = {
 
 def _ping_thread(host, mark, port):
     lostPacket = 0
-    allPacket = 0
-    startTime = time.time()
+    packet_queue = Queue(maxsize=PING_PACKET_HISTORY_LEN)
+
+    IP = host
+    if host.count(':') < 1:     # if not plain ipv6 address, means ipv4 address or hostname
+        try:
+            if PROBE_PROTOCOL_PREFER == 'ipv4':
+                IP = socket.getaddrinfo(host, None, socket.AF_INET)[0][4][0]
+            else:
+                IP = socket.getaddrinfo(host, None, socket.AF_INET6)[0][4][0]
+        except Exception:
+                pass
 
     while True:
+        if packet_queue.full():
+            if packet_queue.get() == 0:
+                lostPacket -= 1
         try:
             b = timeit.default_timer()
-            socket.create_connection((host, port), timeout=1).close()
+            socket.create_connection((IP, port), timeout=1).close()
             pingTime[mark] = int((timeit.default_timer() - b) * 1000)
-        except:
-            lostPacket += 1
-        finally:
-            allPacket += 1
+            packet_queue.put(1)
+        except socket.error as error:
+            if error.errno == errno.ECONNREFUSED:
+                pingTime[mark] = int((timeit.default_timer() - b) * 1000)
+                packet_queue.put(1)
+            #elif error.errno == errno.ETIMEDOUT:
+            else:
+                lostPacket += 1
+                packet_queue.put(0)
 
-        if allPacket > 100:
-            lostRate[mark] = float(lostPacket) / allPacket
-
-        endTime = time.time()
-        if endTime - startTime > 3600:
-            lostPacket = 0
-            allPacket = 0
-            startTime = endTime
+        if packet_queue.qsize() > 30:
+            lostRate[mark] = float(lostPacket) / packet_queue.qsize()
 
         time.sleep(INTERVAL)
 
@@ -133,18 +201,48 @@ def _net_speed():
         netSpeed["avgtx"] = avgtx
         time.sleep(INTERVAL)
 
-def get_load():
-	try:
-		return os.getloadavg()[0]
-	except:
-		return -1.0
-
 def get_realtime_date():
+    t1 = threading.Thread(
+        target=_ping_thread,
+        kwargs={
+            'host': CU,
+            'mark': '10010',
+            'port': PROBEPORT
+        }
+    )
+    t2 = threading.Thread(
+        target=_ping_thread,
+        kwargs={
+            'host': CT,
+            'mark': '189',
+            'port': PROBEPORT
+        }
+    )
+    t3 = threading.Thread(
+        target=_ping_thread,
+        kwargs={
+            'host': CM,
+            'mark': '10086',
+            'port': PROBEPORT
+        }
+    )
     t4 = threading.Thread(
         target=_net_speed,
     )
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t3.setDaemon(True)
     t4.setDaemon(True)
+    t1.start()
+    t2.start()
+    t3.start()
     t4.start()
+
+def get_load():
+    try:
+        return os.getloadavg()[0]
+    except:
+        return 9.9
 
 def byte_str(object):
     '''
@@ -207,10 +305,13 @@ if __name__ == '__main__':
                 CPU = get_cpu()
                 NET_IN, NET_OUT = liuliang()
                 Uptime = get_uptime()
-                Load = get_load()
+                Load_1 = get_load()
+                Load_5 = get_load()
+                Load_15 = get_load()
                 MemoryTotal, MemoryUsed = get_memory()
                 SwapTotal, SwapUsed = get_swap()
                 HDDTotal, HDDUsed = get_hdd()
+                IP_STATUS = ip_status()
 
                 array = {}
                 if not timer:
@@ -220,7 +321,9 @@ if __name__ == '__main__':
                     timer -= 1*INTERVAL
 
                 array['uptime'] = Uptime
-                array['load'] = Load
+                array['load_1'] = Load_1
+                array['load_5'] = Load_5
+                array['load_15'] = Load_15
                 array['memory_total'] = MemoryTotal
                 array['memory_used'] = MemoryUsed
                 array['swap_total'] = SwapTotal
@@ -232,6 +335,14 @@ if __name__ == '__main__':
                 array['network_tx'] = netSpeed.get("nettx")
                 array['network_in'] = NET_IN
                 array['network_out'] = NET_OUT
+                array['ip_status'] = IP_STATUS
+                array['ping_10010'] = lostRate.get('10010') * 100
+                array['ping_189'] = lostRate.get('189') * 100
+                array['ping_10086'] = lostRate.get('10086') * 100
+                array['time_10010'] = pingTime.get('10010')
+                array['time_189'] = pingTime.get('189')
+                array['time_10086'] = pingTime.get('10086')
+                array['tcp'], array['udp'], array['process'], array['thread'] = tupd()
 
                 s.send(byte_str("update " + json.dumps(array) + "\n"))
         except KeyboardInterrupt:
